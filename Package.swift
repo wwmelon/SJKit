4532cfd686d18223cf6de7f9f848ae9081831ade
Shijia Chen <shijia.chen@farfetch.com>
2022-07-19 14:44:22 +0800

    remove image

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com:SnapKit/SnapKit.git", .exact("5.0.1")),
+        .package(url: "git@github.com:mxcl/PromiseKit.git", .exact("6.17.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SJKit",
-            dependencies: ["SnapKit"],
+            dependencies: ["SnapKit", "PromiseKit"],
             path: "SJKit"),
     ]
 )
